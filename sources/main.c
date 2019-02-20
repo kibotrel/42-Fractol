2d7076a47c72f88174a09c97cff22b4171dc3c35
Kilian BOTREL <kibotrel@e1r7p1.42.fr>
2019-02-20 20:33:34 +0100

    Added fractal name checker

--- sources/main.c
+++ sources/main.c
@@ -6,11 +6,12 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 18:52:30 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/02/20 19:28:46 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/02/20 20:32:36 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "env.h"
 
 static void	print_usage(void)
 {
@@ -18,7 +19,7 @@ static void	print_usage(void)
 	ft_putendl("\n\tAvailable fractals:\n\n\t- \033[0;31;1mNOT HANDLED YET\033[0;36m");
 	ft_putendl("\n\033[0m\033[32;1mError handling:\033[0;36m");
 	ft_putendl("\n\tEach following error code are handled by the program");
-	ft_putendl("\n\t- Failed memory allocation (1) \033[0;31;1mNOT HANDLED YET\033[0;36m\n\t- Invalid fractal (2) \033[0;31;1mNOT HANDLED YET\033[0;36m");
+	ft_putendl("\n\t- Invalid fractal (1)\n\t- Failed memory allocation (2) \033[0;31;1mNOT HANDLED YET");
 	ft_putendl("\n\tUse \"echo $?\" to know the last process returned value");
 	ft_putendl("\n\033[0m\033[32;1mControls:\033[0;36m");
 	ft_putendl("\n\tThe following inputs are handled in the mlx window");
@@ -29,11 +30,21 @@ static void	print_usage(void)
 	ft_putendl("\n\t\t\t\t\t\t\033[0m\033[32;1mMade by kibotrel\n\033[0m");
 }
 
-int		main(int ac, char **av)
+static int	valid_fractal(char *name)
+{
+	if (!ft_strcmp(ft_strlowcase(name), "mandelbrot"))
+		return (1);
+	return (0);
+}
+
+int			main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		ft_putendl(av[1]);
+		if (!valid_fractal(av[1]))
+			ft_print_error(ERR_NAME, 1);
+		else
+			ft_putendl("W.I.P");
 	}
 	else
 		print_usage();
