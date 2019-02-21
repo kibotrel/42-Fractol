1083adf232faa77ddf4017a092c1e7a7dea42946
Kilian BOTREL <kibotrel@e2r13p12.42.fr>
2019-02-21 19:36:18 +0100

    Basic inputs handling

---
+++ srcs/hooks.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hooks.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/21 18:28:43 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/02/21 19:18:34 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "mlx.h"
+#include "env.h"
+#include "fractol.h"
+
+static int		red_cross(void *env)
+{
+	free_all(env);
+	exit(0);
+	return (0);
+}
+
+static int		read_input(int key, void *env)
+{
+	if (key == ESC)
+		red_cross(env);
+	// if (key == PLUS || key == MINUS)
+	// 	zoom(env, key);
+	// if (key == W || key == A || key == S || key == D)
+	// 	offset(env, key);
+	// if (key == Z || key == X || key == C)
+	// 	colorset(env, key);
+	// if (key == H)
+	// 	toggle_hud(env);
+	return (0);
+}
+
+void			hooks(t_env *env)
+{
+	mlx_hook(env->mlx->win, 17, 0, red_cross, env);
+	mlx_hook(env->mlx->win, 2, 0, read_input, env);
+}
