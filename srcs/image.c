e306802181e94c8ce57b92a24c8694fc60b36a13
Kilian BOTREL <kibotrel@e2r4p3.42.fr>
2019-03-01 21:30:09 +0100

    Added a function to destroy and get a brand new image

--- srcs/image.c
+++ srcs/image.c
@@ -6,12 +6,22 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 18:30:44 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/02/22 18:32:28 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/01 17:29:42 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "mlx.h"
 #include "env.h"
 #include "fractol.h"
+
+void	new_img(t_env *env)
+{
+	mlx_destroy_image(env->mlx->id, env->mlx->img->id);
+	env->mlx->img->id = mlx_new_image(env->mlx->id, WIDTH, HEIGHT);
+	env->mlx->img->data = mlx_get_data_addr(env->mlx->img->id,
+		&(env->mlx->img->bpp), &(env->mlx->img->size),
+		&(env->mlx->img->endian));
+}
 
 void	pixel_to_image(t_env *env, int x, int y, int color)
 {
