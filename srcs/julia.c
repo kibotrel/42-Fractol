6cfb7338b7e49ceaabca3d7d23f6402fed298f96
Kilian BOTREL <kibotrel@e2r5p1.42.fr>
2019-03-05 08:39:19 +0100

    Added Julia Set

---
+++ srcs/julia.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   julia.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/05 03:42:52 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/03/05 07:51:57 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <math.h>
+#include "env.h"
+#include "fractol.h"
+
+static void		px_setup(t_par *data, double y, double x)
+{
+	data->n = -1;
+	data->cur_y = map(data->y, fill_data(0.0, HEIGHT, (-2.0 + y), (2.0 + y)));
+	data->cur_x = map(data->x, fill_data(0.0, WIDTH, (-2.0 + x), (2.0 + x)));
+}
+
+static void		px_iterate(t_par *data, t_env *env)
+{
+	data->z_x = real_part(data->cur_y, data->cur_x);
+	data->z_y = imaginary_part(data->cur_y, data->cur_x);
+	data->cur_y = data->z_x + env->julia_y;
+	data->cur_x = data->z_y + env->julia_x;
+}
+void			julia(t_env *env)
+{
+	t_par	data;
+
+	data.y = -1;
+	while (++data.y < HEIGHT)
+	{
+		data.x = -1;
+		while (++data.x < WIDTH)
+		{
+			px_setup(&data, env->cam->offset_y, env->cam->offset_x);
+			while (mod(data.cur_y, data.cur_x) < LIM && ++data.n < env->checks)
+				px_iterate(&data, env);
+			if (data.n == env->checks)
+				pixel_to_image(env, data.y, data.x, BLACK);
+			else if (env->cam->color == 1)
+				pixel_to_image(env, data.y, data.x, color(data, env));
+			else if (!env->cam->shift)
+				pixel_to_image(env, data.y, data.x, env->palette[data.n % 16]);
+			else
+				pixel_to_image(env, data.y, data.x, env->palette[data.n % 5]);
+		}
+	}
+}
--- srcs/selector.c
+++ srcs/selector.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 14:09:57 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/01 17:11:18 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/05 03:46:54 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 #include "env.h"
 #include "fractol.h"
 
+void	color_preset(t_env *env)
+{
+	if (env->fractal == MANDELBROT)
+		mandelbrot_color(env);
+	if (env->fractal == JULIA)
+		mandelbrot_color(env);
+}
+
 void	draw_fractal(t_env *env)
 {
 	if (env->fractal == MANDELBROT)
 		mandelbrot(env);
+	if (env->fractal == JULIA)
+		julia(env);
 	mlx_put_image_to_window(env->mlx->id, env->mlx->win, env->mlx->img->id, 0, 0);
 }
