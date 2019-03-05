a367bcf88351e4654bbdb0b5351abdb76b8424d2
Kilian BOTREL <kibotrel@e2r12p23.42.fr>
2019-03-05 13:40:19 +0100

    First implementation of Julia settings via mouse movements

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 18:28:43 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/05 08:05:12 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/05 12:28:53 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,24 @@ static int		red_cross(void *env)
 {
 	free_all(env);
 	exit(0);
+	return (0);
+}
+
+static int		mouse_move(int x, int y, t_env *env)
+{
+	if (env->fractal == JULIA)
+	{
+		new_img(env);
+		if (x < 1280)
+			env->julia_x = map(x, fill_data(0.0, 1279, -1, 0));
+		else
+			env->julia_x = map(x, fill_data(1280, 2560, 0, 1));
+		if (y < 720)
+			env->julia_y = map(y, fill_data(0.0, 719, -1, 0));
+		else
+			env->julia_y = map(y, fill_data(720, 1440, 0, 1));
+		draw_fractal(env);
+	}
 	return (0);
 }
 
@@ -47,6 +65,7 @@ static int		read_input(int key, t_env *env)
 
 void			hooks(t_env *env)
 {
-	mlx_hook(env->mlx->win, 17, 0, red_cross, env);
 	mlx_hook(env->mlx->win, 2, 0, read_input, env);
+	mlx_hook(env->mlx->win, 6, 0, mouse_move, env);
+	mlx_hook(env->mlx->win, 17, 0, red_cross, env);
 }
