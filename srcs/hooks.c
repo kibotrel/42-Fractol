a15439222df93b0a2552d7841dc91c3d7e94e654
Kilian BOTREL <kibotrel@e2r5p1.42.fr>
2019-03-05 08:38:06 +0100

    Added few inputs

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 18:28:43 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/01 21:11:16 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/05 08:05:12 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static int		red_cross(void *env)
 	return (0);
 }
 
-static int		read_input(int key, void *env)
+static int		read_input(int key, t_env *env)
 {
 	if (key == ESC)
 		red_cross(env);
@@ -30,12 +30,16 @@ static int		read_input(int key, void *env)
 		color_mode(env);
 	if (key == PG_UP || key == PG_DOWN)
 		details(env, key);
-	// if (key == PLUS || key == MINUS)
-	// 	zoom(env, key);
-	 if (key == W || key == A || key == S || key == D)
+	if (key == W || key == A || key == S || key == D)
 		offset(env, key);
-	// if (key == Z || key == X || key == C)
-	// 	colorset(env, key);
+	if ((key == Z || key == X || key == C) && env->cam->color == AREA)
+		colorset(env, key);
+	if ((key == V || key == B || key == N) && env->cam->color == GRADIENT)
+		base_color(env, key);
+	if ((key == P && env->cam->color == AREA))
+		psycho_effect(env);
+	if (key == R)
+		reset(env);
 	// if (key == H)
 	// 	toggle_hud(env);
 	return (0);
