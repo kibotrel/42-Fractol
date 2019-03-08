f05644b01ae45ac25bf0d07a24a400eafc35e211
Kilian BOTREL <kibotrel@e2r9p4.42.fr>
2019-03-08 07:19:02 +0100

    Set the child PID to 0 to avoid exceptions

--- srcs/setup.c
+++ srcs/setup.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 15:28:26 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/05 12:24:31 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/08 01:06:46 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,7 @@ static void	init_env(t_env *env, int fractal)
 	env->base_color = BLACK;
 	env->julia_x = 0.0;
 	env->julia_y = 0.0;
+	env->child = 0;
 	color_preset(env);
 }
 
