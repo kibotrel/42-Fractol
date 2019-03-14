1a4bf97a1d61ed62a95a8e4798035d6f711dc567
Kilian BOTREL <kibotrel@e2r13p13.42.fr>
2019-03-14 07:04:19 +0100

    Updated input list, added mouse scroll and movement handling + update offset, reset and added zoom

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 18:28:43 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/08 07:13:51 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/14 01:58:26 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,55 +16,61 @@
 #include "env.h"
 #include "fractol.h"
 
-static int		red_cross(void *env)
+
+static int		mouse_press(int	button, int x, int y, t_env *env)
 {
-	free_all(env);
-	exit(0);
+	if (button == SCROLL_UP)
+		zoom(ZOOM_IN, x, y, env);
+	if (button == SCROLL_DOWN)
+		zoom(ZOOM_OUT, x, y, env);
 	return (0);
 }
-
 static int		mouse_move(int x, int y, t_env *env)
 {
-	if (env->fractal == JULIA)
+	if (env->fractal == JULIA && env->toggle_julia == 1)
 	{
 		new_img(env);
-		if (x < 1180)
-			env->julia_x = map(x, fill_data(-100, 1179, -1, 0));
-		else
-			env->julia_x = map(x, fill_data(1180, 2460, 0, 1));
-		if (y < 0)
-			env->julia_y = map(y, fill_data(-741, -1, -1, 0));
-		else
-			env->julia_y = map(y, fill_data(0, 698, 0, 1));
+		if (x >= 0 && x < WIDTH / 2 && y >= 0 && y <= HEIGHT)
+			env->julia_x = map(x, fill_data(0, WIDTH / 2 - 1, -1, 0));
+		else if (x > WIDTH / 2 && x <= WIDTH && y >= 0 && y <= HEIGHT)
+			env->julia_x = map(x, fill_data(WIDTH / 2, WIDTH, 0, 1));
+		if (y >= 0 && y < HEIGHT / 2 && x >= 0 && x <= WIDTH)
+			env->julia_y = map(y, fill_data(0, HEIGHT / 2 - 1, -1, 0));
+		else if (y > HEIGHT / 2 && y <= HEIGHT && x >= 0 && x <= WIDTH)
+			env->julia_y = map(y, fill_data(HEIGHT / 2, HEIGHT, 0, 1));
 		draw_fractal(env);
 	}
 	return (0);
 }
 
-static int		press(int key, t_env *env)
+static int		key_press(int key, t_env *env)
 {
 	if (key == ESC)
 		red_cross(env);
-	if (key == M)
+	else if (key == M)
 		color_mode(env);
-	if (key == PG_UP || key == PG_DOWN)
+	else if (key == PG_UP || key == PG_DOWN)
 		details(env, key);
-	if (key == W || key == A || key == S || key == D)
+	else if (key == W || key == A || key == S || key == D)
 		offset(env, key);
-	if ((key == Z || key == X || key == C) && env->cam->color == AREA)
+	else if ((key == Z || key == X || key == C) && env->cam->color == AREA)
 		colorset(env, key);
-	if ((key == V || key == B || key == N) && env->cam->color == GRADIENT)
+	else if ((key == V || key == B || key == N) && env->cam->color == GRADIENT)
 		base_color(env, key);
-	if ((key == P && env->cam->color == AREA))
+	else if ((key == P && env->cam->color == AREA))
 		psycho_effect(env);
-	if (key == R)
+	else if (key == R)
 		reset(env);
-	// if (key == H)
-	// 	toggle_hud(env);
+	else if (key == TILDE)
+		env->toggle_julia *= -1;
+	else if (key == H)
+		env->cam->hud *= -1;
+	// else if (key == J || key == K || key == L)
+	// 	change_sound(env, key);
 	return (0);
 }
 
-static int		release(int key, t_env *env)
+static int		key_release(int key, t_env *env)
 {
 	if (key == P && env->child > 0)
 	{
@@ -77,8 +83,9 @@ static int		release(int key, t_env *env)
 
 void			hooks(t_env *env)
 {
-	mlx_hook(env->mlx->win, 2, 0, press, env);
-	mlx_hook(env->mlx->win, 3, 0, release, env);
+	mlx_hook(env->mlx->win, 2, 0, key_press, env);
+	mlx_hook(env->mlx->win, 3, 0, key_release, env);
+	mlx_hook(env->mlx->win, 4, 0, mouse_press, env);
 	mlx_hook(env->mlx->win, 6, 0, mouse_move, env);
 	mlx_hook(env->mlx->win, 17, 0, red_cross, env);
 }
--- srcs/process_input.c
+++ srcs/process_input.c
@@ -6,27 +6,31 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 22:30:56 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/08 05:23:16 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/14 06:46:36 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include "libft.h"
 #include "env.h"
 #include "fractol.h"
+
+#include <stdio.h>//
+#include <math.h>//
 
 void	offset(t_env *env, int key)
 {
 	new_img(env);
 	if (key == W)
-		env->cam->offset_x += OFFSET;
+		env->cam->offset_x += OFFSET / env->cam->zoom;
 	if (key == A)
-		env->cam->offset_y += OFFSET;
+		env->cam->offset_y += OFFSET / env->cam->zoom;
 	if (key == S)
-		env->cam->offset_x -= OFFSET;
+		env->cam->offset_x -= OFFSET / env->cam->zoom;
 	if (key == D)
-		env->cam->offset_y -= OFFSET;
+		env->cam->offset_y -= OFFSET / env->cam->zoom;
 	draw_fractal(env);
 }
 
@@ -42,6 +46,8 @@ void	details(t_env *env, int key)
 
 void	psycho_effect(t_env *env)
 {
+	char	*command;
+
 	if (!env->child)
 		env->child = fork();
 	if (env->child > 0)
@@ -55,15 +61,25 @@ void	psycho_effect(t_env *env)
 	}
 	else if (env->child == 0)
 		while (1)
-			system("afplay -q 1 sound.wav");
+		{
+			command = ft_strjoin("afplay -q 1 ", env->sound_name);
+			system(command);
+			free(command);
+		}
 }
 
 void	reset(t_env *env)
 {
 	new_img(env);
-	env->checks = 100;
+	env->checks = 50;
 	env->cam->offset_y = 0.0;
 	env->cam->offset_x = 0.0;
+	env->y_max = 2;
+	env->y_min = -2;
+	env->x_max = 2;
+	env->x_min = -2;
+	env->zoom_count = 1;
+	env->cam->zoom = 1;
 	if (env->cam->shift == NEON)
 		set1(env);
 	else if (env->cam->shift == RED_ORANGE)
@@ -73,4 +89,27 @@ void	reset(t_env *env)
 	else
 		color_preset(env);
 	draw_fractal(env);
+}
+
+void	zoom(int direction, int x, int y, t_env *env)
+{
+	if (y >= 0 && y <= HEIGHT && x >= 0 && x <= WIDTH)
+	{
+		new_img(env);
+		if (direction == ZOOM_IN)
+		{
+			env->zoom_count++;
+			env->cam->zoom *= 1.1;
+		}
+		else if (direction == ZOOM_OUT && env->zoom_count > 1)
+		{
+			env->zoom_count--;
+			env->cam->zoom /= 1.1;
+		}
+		env->y_min = -2 + (4 / 2) - (4 / 2 / env->cam->zoom);
+		env->y_max = env->y_min + (4 / env->cam->zoom);
+		env->x_min = -2 + (4 / 2) - (4 /  2 / env->cam->zoom);
+		env->x_max = env->x_min + (4 /env->cam->zoom);
+		draw_fractal(env);
+	}
 }
