b6d72e36fa8d6397295c3ece69680b3a9f114fe4
Kilian BOTREL <kibotrel@e2r8p16.42.fr>
2019-03-16 07:19:12 +0100

    First HUD implementation, might change in the future

---
+++ srcs/hud.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hud.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/14 09:01:34 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/03/16 06:17:35 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "mlx.h"
+#include "libft.h"
+#include "env.h"
+#include "fractol.h"
+
+void		hud_background(t_env *env)
+{
+	int	x;
+	int	y;
+
+	y = -1;
+	while (++y <= HEIGHT)
+	{
+		x = 799;
+		while (++x <= WIDTH)
+		{
+			if (y < 50 || (y >= 400 && y < 450))
+				pixel_to_image(env, x, y, 0x23272A);
+			else
+				pixel_to_image(env, x, y, 0x2C2F33);
+		}
+	}
+}
+
+static void	fill_fields(t_infos *i, t_env *env)
+{
+	double	mix;
+	double	miy;
+	double	max;
+	double	may;
+
+	mix = env->x_min + (float)env->cam->offset_y;
+	miy = env->y_min + (float)env->cam->offset_x;
+	max = env->x_max + (float)env->cam->offset_y;
+	may = env->y_max + (float)env->cam->offset_x;
+	sprintf(i->checks, "Iterations    : %d", env->checks);
+	sprintf(i->zoom, "Zoom          : x%.2f", env->cam->zoom);
+	sprintf(i->c, "%+.3f %+.3fi", env->julia_x, env->julia_y);
+	sprintf(i->off_x, "Offset X      : %.2f", env->cam->offset_y);
+	sprintf(i->off_y, "Offset Y      : %.2f", env->cam->offset_x);
+	sprintf(i->sound, "Sound name    : %s\n", env->sound_name);
+	sprintf(i->region, "%+.3f %+.3fi to %+.3f %+.3fi", mix, miy, max, may);
+}
+
+static void	fractal_infos(void *id, void *win, t_env *env)
+{
+	t_infos i;
+
+	fill_fields(&i, env);
+	get_fractal_name(env->mlx->id, env->mlx->win, env);
+	mlx_string_put(id, win, 1040, 15, WHITE, "Informations");
+	mlx_string_put(id, win, 955, 100, WHITE, i.checks);
+	if (ft_strlen(i.zoom) < LEN_ZOOM_MAX)
+		mlx_string_put(id, win, 955, 120, WHITE, i.zoom);
+	else
+	{
+		mlx_string_put(id, win, 955, 120, WHITE, "Zoom          :");
+		mlx_string_put(id, win, 1115, 120, TRUE_RED, "Too big to fit in !");
+	}
+	mlx_string_put(id, win, 955, 140, WHITE, i.off_x);
+	mlx_string_put(id, win, 955, 160, WHITE, i.off_y);
+	infos_color(env, env->mlx->id, env->mlx->win);
+	mlx_string_put(id, win, 955, 220, WHITE, i.sound);
+	mlx_string_put(id, win, 1025, 260, WHITE, "Explored region");
+	mlx_string_put(id, win, 940, 280, WHITE, i.region);
+	if (env->fractal == JULIA || env->fractal == BURNING_JULIA)
+	{
+		mlx_string_put(id, win, 1030, 320, WHITE, "Preset complex");
+		mlx_string_put(id, win, 1030, 340, WHITE, i.c);
+	}
+}
+
+static void	fractal_controls(void *id, void *win)
+{
+	mlx_string_put(id, win, 1065, 415, WHITE, "Controls");
+	mlx_string_put(id, win, 835, 480, WHITE, "Color Modes     : M");
+	mlx_string_put(id, win, 1125, 480, WHITE, "Reset View      : R");
+	mlx_string_put(id, win, 835, 500, WHITE, "Offset          : W/A/S/D");
+	mlx_string_put(id, win, 1125, 500, WHITE, "Zoom            : Scroll");
+	mlx_string_put(id, win, 970, 560, WHITE, "Details : Page_Up/Page_Down");
+	mlx_string_put(id, win, 835, 620, WHITE, "Gradient Colors : V/B/N");
+	mlx_string_put(id, win, 1125, 620, WHITE, "Area Colors     : Z/X/C");
+	mlx_string_put(id, win, 835, 640, WHITE, "Config Julia    : Tilde");
+	mlx_string_put(id, win, 1125, 640, WHITE, "Psycho mode     : P");
+	mlx_string_put(id, win, 835, 660, WHITE, "Change sound    : J/K/L");
+	mlx_string_put(id, win, 1125, 660, WHITE, "Select fractal  : 1/2/3");
+	mlx_string_put(id, win, 970, 720, WHITE, "Quit : Escape / Windowcross");
+	mlx_string_put(id, win, 1200, 765, WHITE, "Fractol by kibotrel");
+}
+
+void		hud_infos(t_env *env)
+{
+	fractal_infos(env->mlx->id, env->mlx->win, env);
+	fractal_controls(env->mlx->id, env->mlx->win);
+}
---
+++ srcs/infos_colors.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   infos_colors.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/16 05:15:31 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/03/16 05:49:57 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+#include "env.h"
+#include "fractol.h"
+
+static void	infos_area(t_env *env, void *id, void *win)
+{
+	mlx_string_put(id, win, 955, 200, WHITE, "Color palette :");
+	if (env->cam->shift == DEFAULT)
+		mlx_string_put(id, win, 1115, 200, WHITE, "Default");
+	else if (env->cam->shift == PASTEL)
+		mlx_string_put(id, win, 1115, 200, WHITE, "Pastel");
+	else if (env->cam->shift == RED_ORANGE)
+		mlx_string_put(id, win, 1115, 200, WHITE, "Fire");
+	else
+		mlx_string_put(id, win, 1115, 200, WHITE, "Forest");
+}
+
+static void	infos_gradient(t_env *env, void *id, void *win)
+{
+	mlx_string_put(id, win, 955, 200, WHITE, "Color shade   :");
+	if (env->base_color == BLACK)
+		mlx_string_put(id, win, 1115, 200, WHITE, "Default");
+	if (env->base_color == BLUE)
+		mlx_string_put(id, win, 1115, 200, WHITE, "Blue");
+	if (env->base_color == RED)
+		mlx_string_put(id, win, 1115, 200, WHITE, "Red");
+}
+
+void		infos_color(t_env *env, void *id, void *win)
+{
+	mlx_string_put(id, win, 955, 180, WHITE, "Color mode    :");
+	if (env->cam->color == GRADIENT)
+		mlx_string_put(id, win, 1115, 180, WHITE, "Gradient");
+	else
+		mlx_string_put(id, win, 1115, 180, WHITE, "Areas");
+	if (env->cam->color == AREA)
+		infos_area(env, env->mlx->id, env->mlx->win);
+	else
+		infos_gradient(env, env->mlx->id, env->mlx->win);
+}
