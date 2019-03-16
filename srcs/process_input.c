350ea09850925c38b0b0754b7fc10baa9468e6eb
Kilian BOTREL <kibotrel@e2r8p16.42.fr>
2019-03-16 07:16:06 +0100

    Modified zoom added more parameters on reset event and handling sound changement

--- srcs/process_input.c
+++ srcs/process_input.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 22:30:56 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/14 06:46:36 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/16 07:09:34 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,16 @@
 #include "env.h"
 #include "fractol.h"
 
-#include <stdio.h>//
-#include <math.h>//
-
 void	offset(t_env *env, int key)
 {
 	new_img(env);
-	if (key == W)
+	if (key == W && env->cam->offset_x < OFFSET_MAX)
 		env->cam->offset_x += OFFSET / env->cam->zoom;
-	if (key == A)
+	else if (key == A && env->cam->offset_y < OFFSET_MAX)
 		env->cam->offset_y += OFFSET / env->cam->zoom;
-	if (key == S)
+	else if (key == S && env->cam->offset_x > OFFSET_MIN)
 		env->cam->offset_x -= OFFSET / env->cam->zoom;
-	if (key == D)
+	else if (key == D && env->cam->offset_y > OFFSET_MIN)
 		env->cam->offset_y -= OFFSET / env->cam->zoom;
 	draw_fractal(env);
 }
@@ -37,10 +34,10 @@ void	offset(t_env *env, int key)
 void	details(t_env *env, int key)
 {
 	new_img(env);
-	if (key == PG_UP)
-		env->checks += 10;
-	if (key == PG_DOWN && env->checks >= 20)
-		env->checks -= 10;
+	if (key == PG_UP && env->checks <= MAX_LOOPS)
+		env->checks += 5;
+	else if (key == PG_DOWN && env->checks >= 20)
+		env->checks -= 5;
 	draw_fractal(env);
 }
 
@@ -71,7 +68,7 @@ void	psycho_effect(t_env *env)
 void	reset(t_env *env)
 {
 	new_img(env);
-	env->checks = 50;
+	env->checks = 25;
 	env->cam->offset_y = 0.0;
 	env->cam->offset_x = 0.0;
 	env->y_max = 2;
@@ -80,36 +77,71 @@ void	reset(t_env *env)
 	env->x_min = -2;
 	env->zoom_count = 1;
 	env->cam->zoom = 1;
-	if (env->cam->shift == NEON)
+	if (env->cam->shift == PASTEL)
 		set1(env);
 	else if (env->cam->shift == RED_ORANGE)
 		set2(env);
-	else if (env->cam->shift == FACEBOOK)
+	else if (env->cam->shift == FOREST)
 		set3(env);
 	else
 		color_preset(env);
 	draw_fractal(env);
 }
 
+#include <stdio.h>
+
 void	zoom(int direction, int x, int y, t_env *env)
 {
-	if (y >= 0 && y <= HEIGHT && x >= 0 && x <= WIDTH)
+	if (y >= 0 && y <= 800 && x >= 0 && x <= 800)
 	{
 		new_img(env);
-		if (direction == ZOOM_IN)
+		if (direction == ZOOM_IN && env->zoom_count < ZOOM_MAX)
 		{
 			env->zoom_count++;
 			env->cam->zoom *= 1.1;
+			if (x > 0 && x < 400)
+			{
+				env->cam->offset_y -= (OFFSET / env->cam->zoom) * (x / 256);
+				if (y > 0 && y < 400)
+					env->cam->offset_x -= (OFFSET / env->cam->zoom) * (y / 256);
+				else if (y > 400 && y < 800)
+					env->cam->offset_x += (OFFSET / env->cam->zoom) * (y / 256);
+			}
+			else if (x >= 400 && x < 800)
+			{
+				env->cam->offset_y += OFFSET / (env->cam->zoom) * (x / 256);
+				if (y >= 0 && y < 400)
+					env->cam->offset_x -= (OFFSET / env->cam->zoom) * (y / 256);
+				else if (y > 400 && y < 800)
+					env->cam->offset_x += (OFFSET / env->cam->zoom) * (y / 256);
+			}
 		}
 		else if (direction == ZOOM_OUT && env->zoom_count > 1)
 		{
 			env->zoom_count--;
-			env->cam->zoom /= 1.1;
+			if ((env->cam->zoom /= 1.1) < 1)
+				env->cam->zoom = 1;
+			if (x >= 0 && x < 400)
+			{
+				env->cam->offset_y += (OFFSET / env->cam->zoom) * (x / 256);
+				if (y >= 0 && y < 400)
+					env->cam->offset_x += (OFFSET / env->cam->zoom) * (y / 256);
+				else
+					env->cam->offset_x -= (OFFSET / env->cam->zoom) * (y / 256);
+			}
+			else
+			{
+				env->cam->offset_y -= OFFSET / (env->cam->zoom) * (x / 256);
+				if (y >= 0 && y < 400)
+					env->cam->offset_x += (OFFSET / env->cam->zoom) * (y / 256);
+				else
+					env->cam->offset_x -= (OFFSET / env->cam->zoom) * (y / 256);
+			}
 		}
 		env->y_min = -2 + (4 / 2) - (4 / 2 / env->cam->zoom);
 		env->y_max = env->y_min + (4 / env->cam->zoom);
-		env->x_min = -2 + (4 / 2) - (4 /  2 / env->cam->zoom);
-		env->x_max = env->x_min + (4 /env->cam->zoom);
+		env->x_min = -2 + (4 / 2) - (4 / 2 / env->cam->zoom);
+		env->x_max = env->x_min + (4 / env->cam->zoom);
 		draw_fractal(env);
 	}
 }
---
+++ srcs/process_input2.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_input2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/14 08:48:14 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/03/16 06:11:11 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+#include "fractol.h"
+
+void	change_sound(t_env *env, int key)
+{
+	new_img(env);
+	if (key == J)
+		env->sound_name = "sound.wav";
+	else if (key == K)
+		env->sound_name = "sound2.wav";
+	else if (key == L)
+		env->sound_name = "sound3.wav";
+	draw_fractal(env);
+}
