30942df3ea161df1102d1d2b60b3516732140bef
Kilian BOTREL <kibotrel@e3r6p1.42.fr>
2019-03-18 07:49:36 +0100

    Added a function to set up default iteration depending on the given fractal + added sierpinski triangle to the selector

--- srcs/selector.c
+++ srcs/selector.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 14:09:57 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/16 06:09:05 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/18 07:07:38 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,22 @@
 
 void	color_preset(t_env *env)
 {
-	if (env->fractal == MANDELBROT || env->fractal == JULIA
+	if (env->fractal == MANDELBROT
+		|| env->fractal == JULIA
 		|| env->fractal == BURNING_JULIA)
 		mandelbrot_julia_color(env);
+}
+
+void	set_checks(t_env *env)
+{
+	if (env->fractal == MANDELBROT)
+		env->checks = 15;
+	else if (env->fractal == JULIA || env->fractal == BURNING_JULIA)
+		env->checks = 25;
+	else if (env->fractal == BURNING_SHIP)
+		env->checks = 20;
+	else if (env->fractal == SIERPINSKI)
+		env->checks = 0;
 }
 
 void	change_fractal(t_env *env, int key)
@@ -31,20 +44,15 @@ void	change_fractal(t_env *env, int key)
 		env->fractal = BURNING_SHIP;
 	else if (key == NUM_4)
 		env->fractal = BURNING_JULIA;
+	else if (key == NUM_5)
+		env->fractal = SIERPINSKI;
 	env->cam->shift = DEFAULT;
 	color_preset(env);
 	reset(env);
 }
 
-void	draw_fractal(t_env *env)
+void	draw_fractal(t_env *env, void *id, void *win, void *img)
 {
-	void	*id;
-	void	*win;
-	void	*img;
-
-	id = env->mlx->id;
-	win = env->mlx->win;
-	img = env->mlx->img->id;
 	if (env->fractal == MANDELBROT)
 		mandelbrot(env);
 	else if (env->fractal == JULIA)
@@ -53,6 +61,8 @@ void	draw_fractal(t_env *env)
 		burning_ship(env);
 	else if (env->fractal == BURNING_JULIA)
 		burning_julia(env);
+	else if (env->fractal == SIERPINSKI)
+		sierpinski(env);
 	hud_background(env);
 	mlx_put_image_to_window(id, win, img, 0, 0);
 	hud_infos(env);
