6f5e5be89b7de482e7792ab87abe49c04f8e6bd1
Kilian BOTREL <kibotrel@e3r6p1.42.fr>
2019-03-18 07:51:58 +0100

    Renamed zoom to zoom_mouse since there is a keyboard_zoom now

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 18:28:43 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/18 02:19:08 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/18 07:09:02 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@
 static int		mouse_press(int button, int x, int y, t_env *env)
 {
 	if (button == SCROLL_UP)
-		zoom(ZOOM_IN, x, y, env);
+		zoom_mouse(ZOOM_IN, x, y, env);
 	else if (button == SCROLL_DOWN)
-		zoom(ZOOM_OUT, x, y, env);
+		zoom_mouse(ZOOM_OUT, x, y, env);
 	return (0);
 }
 
@@ -66,8 +66,10 @@ static int		key_press(int key, t_env *env)
 		env->toggle_julia *= -1;
 	else if (key == J || key == K || key == L)
 		change_sound(env, key);
-	else if (key == NUM_1 || key == NUM_2 || key == NUM_3 || key == NUM_4)
+	else if (key == NUM_1 || key == NUM_2 || key == NUM_3 || key == NUM_4 || key == NUM_5)
 		change_fractal(env, key);
+	else if (key == PLUS || key == MINUS)
+		keyboard_zoom(env, key);
 	return (0);
 }
 
