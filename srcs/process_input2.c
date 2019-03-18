71fc929054c7751dccbe1e656a24f841dc09d725
Kilian BOTREL <kibotrel@e3r6p1.42.fr>
2019-03-18 07:53:09 +0100

    Added keyboard_zoom

--- srcs/process_input2.c
+++ srcs/process_input2.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 08:48:14 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/18 02:22:12 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/18 07:05:54 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,23 @@ void	change_sound(t_env *env, int key)
 		env->sound_name = "sound2.wav";
 	else if (key == L)
 		env->sound_name = "sound3.wav";
+	draw_fractal(env, env->mlx->id, env->mlx->win, env->mlx->img->id);
+}
+
+void	keyboard_zoom(t_env *env, int key)
+{
+	new_img(env);
+	if (key == PLUS)
+	{
+		env->zoom_count++;
+		env->cam->zoom *= 1.1;
+	}
+	else if (key == MINUS && env->zoom_count > 1)
+	{
+		env->zoom_count--;
+		if ((env->cam->zoom /= 1.1) < 1)
+			env->cam->zoom = 1;
+	}
+	update_bounds(env);
 	draw_fractal(env, env->mlx->id, env->mlx->win, env->mlx->img->id);
 }
