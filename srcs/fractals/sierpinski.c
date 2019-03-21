0486c27f5445da3dedc04713016f54ccc174c194
Kilian BOTREL <kibotrel@e2r3p6.42.fr>
2019-03-21 23:20:09 +0100

    Fix norm

--- srcs/fractals/sierpinski.c
+++ srcs/fractals/sierpinski.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 02:13:46 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/19 15:21:27 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/21 22:55:18 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "env.h"
 #include "fractol.h"
 
-static t_pos	middle(t_pos a, t_pos b)
+static t_pos	mid(t_pos a, t_pos b)
 {
 	t_pos mid;
 
@@ -24,31 +24,29 @@ static t_pos	middle(t_pos a, t_pos b)
 	return (mid);
 }
 
-static void		recursion(t_pos a, t_pos b, t_pos c, t_env *env, int level)
+static void		depth(t_pos p[3], t_env *env, int lvl)
 {
-	draw_line(a, b, env);
-	draw_line(b, c, env);
-	draw_line(c, a, env);
-	if (level > 0)
+	draw_line(p[0], p[1], env);
+	draw_line(p[1], p[2], env);
+	draw_line(p[2], p[0], env);
+	if (lvl > 0)
 	{
-		recursion(a, middle(a, b), middle(a, c), env, level - 1);
-		recursion(b, middle(a, b), middle(b, c), env, level - 1);
-		recursion(c, middle(a, c), middle(b, c), env, level - 1);
+		depth((t_pos[3]){p[0], mid(p[0], p[1]), mid(p[0], p[2])}, env, lvl - 1);
+		depth((t_pos[3]){p[1], mid(p[0], p[1]), mid(p[1], p[2])}, env, lvl - 1);
+		depth((t_pos[3]){p[2], mid(p[0], p[2]), mid(p[1], p[2])}, env, lvl - 1);
 	}
 }
 
 void			sierpinski(t_env *env)
 {
-	t_pos	a;
-	t_pos	b;
-	t_pos	c;
+	t_pos p[3];
 
-	a.x = 50 * env->cam->zoom - env->cam->offset_y;
-	a.y = 700 * env->cam->zoom - env->cam->offset_x;
-	b.x = 750 * env->cam->zoom - env->cam->offset_y;
-	b.y = 700 * env->cam->zoom - env->cam->offset_x;
-	c.x = a.x + (b.x - a.x) / 2;
-	c.y = a.y - sqrt(pow(b.x - a.x, 2) - pow((b.x - a.x) / 2, 2));
-	recursion(a, b, c, env, env->checks);
+	p[0].x = 50 * env->cam->zoom - env->cam->offset_y;
+	p[0].y = 700 * env->cam->zoom - env->cam->offset_x;
+	p[1].x = 750 * env->cam->zoom - env->cam->offset_y;
+	p[1].y = 700 * env->cam->zoom - env->cam->offset_x;
+	p[2].x = p[0].x + (p[1].x - p[0].x) / 2;
+	p[2].y = p[0].y - sqrt(pow2(p[1].x - p[0].x) - pow2((p[1].x - p[0].x) / 2));
+	depth(p, env, env->checks);
 	hud_background(env);
 }
