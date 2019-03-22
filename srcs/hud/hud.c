602e310dcaace62ac084f7589704a57d970fae57
Kilian BOTREL <kibotrel@e2r3p6.42.fr>
2019-03-22 05:41:55 +0100

    Modified few displayed values for Sierpinski and Koch

--- srcs/hud/hud.c
+++ srcs/hud/hud.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 09:01:34 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/21 23:08:42 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/22 01:43:45 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,15 +47,20 @@ static void	fill_fields(t_infos *i, t_env *env)
 	miy = env->y_min + (float)env->cam->offset_x;
 	max = env->x_max + (float)env->cam->offset_y;
 	may = env->y_max + (float)env->cam->offset_x;
-	sprintf(i->checks, "Iterations    : %d", env->checks);
+	if (env->fractal == SIERPINSKI || env->fractal == KOCH)
+		sprintf(i->checks, "Depth level   : %d", env->checks);
+	else
+		sprintf(i->checks, "Iterations    : %d", env->checks);
 	if (env->zoom_count < ZOOM_MAX)
 		sprintf(i->zoom, "Zoom          : x%.2f", env->cam->zoom);
 	sprintf(i->c, "%+.5f %+.5fi", env->julia_x, env->julia_y);
 	sprintf(i->off_x, "Offset X      : %.2f", env->cam->offset_y);
 	sprintf(i->off_y, "Offset Y      : %.2f", env->cam->offset_x);
-	if (env->fractal != SIERPINSKI)
+	if (env->fractal != SIERPINSKI && env->fractal != KOCH)
+	{
 		sprintf(i->sound, "Sound name    : %s\n", env->sound_name);
-	sprintf(i->region, "%+.5f %+.5fi to %+.5f %+.5fi", mix, miy, max, may);
+		sprintf(i->region, "%+.5f %+.5fi to %+.5f %+.5fi", mix, miy, max, may);
+	}
 }
 
 static void	fractal_infos(void *id, void *win, t_env *env)
@@ -70,7 +75,7 @@ static void	fractal_infos(void *id, void *win, t_env *env)
 	mlx_string_put(id, win, 955, 140, WHITE, i.off_x);
 	mlx_string_put(id, win, 955, 160, WHITE, i.off_y);
 	infos_color(env, env->mlx->id, env->mlx->win);
-	if (env->fractal != SIERPINSKI)
+	if (env->fractal != SIERPINSKI && env->fractal != KOCH)
 		mlx_string_put(id, win, 955, 220, WHITE, i.sound);
 	infos_complex(env, env->mlx->id, env->mlx->win, i);
 }
