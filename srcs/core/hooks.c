5bdd35d0d686da5cb6100aeaf5d5406802374376
Kilian BOTREL <kibotrel@e1r10p14.42.fr>
2019-03-29 17:25:07 +0100

    Small update

--- srcs/core/hooks.c
+++ srcs/core/hooks.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 18:28:43 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/28 03:01:03 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/29 16:27:08 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,9 @@
 
 static int	mouse_press(int button, int x, int y, t_env *env)
 {
-	if (button == SCROLL_UP
-		&& env->fractal != KOCH
-		&& env->fractal != BARNSLEY
-		&& env->fractal != SIERPINSKI)
+	if (button == SCROLL_UP && env->fractal < SIERPINSKI)
 		zoom_mouse(ZOOM_IN, x, y, env);
-	else if (button == SCROLL_DOWN
-		&& env->fractal != KOCH
-		&& env->fractal != BARNSLEY
-		&& env->fractal != SIERPINSKI)
+	else if (button == SCROLL_DOWN && env->fractal < SIERPINSKI)
 		zoom_mouse(ZOOM_OUT, x, y, env);
 	return (0);
 }
@@ -53,8 +47,7 @@ static int	key_press(int key, t_env *env)
 {
 	if (key == ESC)
 		red_cross(env);
-	else if (key == M && env->fractal != KOCH
-		&& env->fractal != BARNSLEY && env->fractal != SIERPINSKI)
+	else if (key == M && env->fractal < SIERPINSKI)
 		color_mode(env);
 	else if (key == PG_UP || key == PG_DOWN)
 		details(env, key);
@@ -72,8 +65,7 @@ static int	key_press(int key, t_env *env)
 		env->toggle_julia *= -1;
 	else if (key == J || key == K || key == L)
 		change_sound(env, key);
-	else if (key == NUM_1 || key == NUM_2 || key == NUM_3 || key == NUM_4
-		|| key == NUM_5 || key == NUM_6 || key == NUM_7 || key == NUM_8)
+	else if (key >= NUM_1 && key <= NUM_8)
 		change_fractal(env, key);
 	else if (key == PLUS || key == MINUS)
 		keyboard_zoom(env, key);
