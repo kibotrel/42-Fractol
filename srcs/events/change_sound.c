d5e32234dac86cb093167825ede82fce5a0463d3
Kilian BOTREL <kibotrel@e2r9p1.42.fr>
2019-03-30 18:02:40 +0100

    Updated sounds names

--- srcs/events/change_sound.c
+++ srcs/events/change_sound.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 10:11:23 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/03/28 03:03:27 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/03/30 17:37:06 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 void	change_sound(t_env *env, int key)
 {
 	if (key == J)
-		env->sound_name = "sounds/sound.wav";
+		env->sound_name = "sounds/default.wav";
 	else if (key == K)
-		env->sound_name = "sounds/sound2.wav";
+		env->sound_name = "sounds/psy.wav";
 	else if (key == L)
-		env->sound_name = "sounds/sound3.wav";
+		env->sound_name = "sounds/zen.wav";
 	threads(env, env->mlx->id, env->mlx->win, env->mlx->img->id);
 }
